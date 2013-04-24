f6721003f420ec05d359528213cf204ca52c9483
Alex Popescu <the.mindstorm@gmail.com>
2013-04-24 16:39:16 -0700

    Removed one of the python init scripts

--- bin/pycomp.py
+++
@@ -1,9 +0,0 @@
-codecomp = False
-try:
-    import readline
-except ImportError:
-    print "No readline"
-else:
-    import rlcompleter
-    readline.parse_and_bind("tab: complete")
-    codecomp = True
