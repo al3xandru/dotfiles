f2e4b866c5b805102de8e27b69af6209786ff801
Alex Popescu <the.mindstorm@gmail.com>
2013-04-25 10:18:44 -0700

    Updated for full path

--- bin/markup.sh
+++ bin/markup.sh
@@ -1,1 +1,9 @@
-/Users/alex/Dropbox/workspace/python/markdown/bin/markup.sh
+#!/bin/sh
+if [ -h $0 ]; then
+    SLINK=`ls -l $0 | awk '{print $11}'`;
+    DIR=`dirname $SLINK`
+else
+    DIR=`dirname $0`
+fi
+#python "$DIR/../mkdn/markuptools.py" "$@"
+python "$HOME/Dropbox/workspace/python/markdown/mkdn/markuptools.py" "$@"
