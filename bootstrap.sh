a77ab11bd32b6f523bfe9f787a7cdace17f0e637
Alex Popescu <the.mindstorm@gmail.com>
2013-05-03 23:21:48 -0700

    added --status flag to compare against target

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,17 +1,47 @@
 #!/bin/bash
 cd "$(dirname "${BASH_SOURCE}")"
 git pull origin master
+function status() {
+    IFS=$'\n'
+    for file in $(ls -A) ; do
+        if [ $file = "bootstrap.sh" ]; then
+            continue
+        fi
+        if [ $file = "README.md" ]; then
+            continue
+        fi
+        if [ $file = ".git" ]; then
+            continue
+        fi
+        echo "Comparing $file"
+        if [ -d $file ]; then
+            diff -r $file $HOME/$file
+        else
+            diff $file $HOME/$file
+        fi
+    done
+}
 function doIt() {
 	rsync --exclude ".git/" --exclude ".DS_Store" --exclude "bootstrap.sh" --exclude "README.md" -av . ~
 }
-if [ "$1" == "--force" -o "$1" == "-f" ]; then
-	doIt
-else
-	read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
-	echo
-	if [[ $REPLY =~ ^[Yy]$ ]]; then
-		doIt
-	fi
-fi
+case "$1" in
+    --status)
+        status
+        ;;
+    --force)
+        doIt
+        ;;
+    -f)
+        doIt
+        ;;
+    *)
+	    read -p "This may overwrite existing files in your home directory. Are you sure? (y/n) " -n 1
+	    echo
+        if [[ $REPLY =~ ^[Yy]$ ]]; then
+            doIt
+        fi
+        ;;
+esac
 unset doIt
+unset status
 source ~/.bash_profile
