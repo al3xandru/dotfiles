4377400381c5b03333226ee9e070859054c57e31
Alex Popescu <the.mindstorm@gmail.com>
2013-05-05 21:47:01 -0700

    improved python shell autocompleter

--- bin/pythonstartup.py
+++ bin/pythonstartup.py
@@ -6,7 +6,20 @@ except ImportError:
   print "Module readline unavailable"
 else:
   import rlcompleter
+  class MyCompleter(rlcompleter.Completer):
+    def complete(self, text, state):
+      if text.lstrip() == '':
+        if state == 0:
+          return text + '\t'
+        else:
+          return None
+      else:
+        return rlcompleter.Completer.complete(self, text, state)
+
   readline.parse_and_bind("tab: complete")
   if sys.platform == 'darwin':
     readline.parse_and_bind("bind ^I rl_complete")
+  readline.set_completer(MyCompleter().complete)
   AP_AUTOCOMPLETE=True
+
+# vim:set tabstop=2 shiftwidth=2 softtabstop=2 :
