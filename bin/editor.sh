9781b106b5e386e4779d6b87a128de5f12c7c01f
Alex Popescu <the.mindstorm@gmail.com>
2013-06-24 12:29:05 -0700

    use either homebrew or default vim

--- bin/editor.sh
+++ bin/editor.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 # http://brettterpstra.com/2013/03/30/a-multi-purpose-editor-variable/
+if [ -f /usr/local/bin/vim ]; then
+    VIM_CMD="/usr/local/bin/vim"
+else
+    VIM_CMD="/usr/bin/vim"
+fi
 case "$1" in
     *_EDITMSG|*MERGE_MSG|*_TAGMSG )
-        /usr/local/bin/vim "$1"
+        $VIM_CMD "$1"
         ;;
     *crontab.* )
-        /usr/local/bin/vim "$1"
+        $VIM_CMD "$1"
         ;;
     *.md|*.markdown|*.mdown|*.mkdown )
         /usr/local/bin/mmdc "$1"
