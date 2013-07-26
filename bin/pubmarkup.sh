a1835a06363334e125772afbc336dd357f75d9b4
Alex Popescu <the.mindstorm@gmail.com>
2013-07-26 12:33:56 -0700

    not sure

--- bin/pubmarkup.sh
+++ bin/pubmarkup.sh
@@ -3,22 +3,24 @@
 SCRIPT="$HOME/Dropbox/workspace/python/markdown/mkdn/markuptools.py"
 NOTIFIER_APP=""
 NOTIFIER_TYPE="0"
-if [ -x "/Applications/terminal-notifier.app/Contents/MacOS/terminal-notifier" ]; then
+if [[ -x "/Applications/terminal-notifier.app/Contents/MacOS/terminal-notifier" ]]; then
     NOTIFIER_TYPE="1"
     NOTIFIER_APP="/Applications/terminal-notifier.app/Contents/MacOS/terminal-notifier"
-elif [ -x "/usr/local/bin/MountainNotifier" ]; then
+elif [[ -x "/usr/local/bin/MountainNotifier" ]]; then
     NOTIFIER_TYPE="2"
     NOTIFIER_APP="/usr/local/bin/MountainNotifier"
 fi
 APP_NAME="com.metaclassy.byword"
-if [ "$1" = "-a" ]; then
+if [[ "$1" = "-a" ]]; then
     shift
-    if [ $NOTIFIER_TYPE = "2" ]; then
+    if [[ $NOTIFIER_TYPE = "1" ]]; then
+        APP_NAME=$1
+    elif [[ $NOTIFIER_TYPE = "2" ]]; then
         APP_NAME=$(grep 'CFBundleIdentifier' -A1 "/Applications/$1.app/Contents/Info.plist" | sed s/string//g | sed s/key//g | sed s/[\<\>\/]//g | tail -1 | tr -d " \t") 
     fi
     shift
 fi
-if [ "$1" = "--env" ]; then
+if [[ "$1" = "--env" ]]; then
     OUTPUT=$(echo "$KMVAR_TextSel" | python "$SCRIPT" --type=m1 --tumblr -)
 elif [ "$1" != "" ]; then
     OUTPUT=$(python "$SCRIPT" --type=m1 --tumblr "$1")
@@ -32,19 +34,19 @@ purl_pattern="post_uri: ([^${IFS}]*)"
 pub_state=""
 purl=""
 notif_msg=""
-if [ $OUTPUT =~ $pub_pattern ]; then
+if [[ $OUTPUT =~ $pub_pattern ]]; then
     pub_state="${BASH_REMATCH[1]}"
     #echo "pub_state: ${BASH_REMATCH[1]} -> $pub_state"
     say "$pub_state"
 fi
-if [ $OUTPUT =~ $tweet_pattern ]; then
+if [[ $OUTPUT =~ $tweet_pattern ]]; then
     notif_msg="$notif_msg${IFS}Tweet: ${BASH_REMATCH[1]}"
 fi
-if [ $OUTPUT =~ $pid_pattern ]; then
+if [[ $OUTPUT =~ $pid_pattern ]]; then
     #echo "id: ${BASH_REMATCH[1]}"
     notif_msg="$notif_msg${IFS}Id: ${BASH_REMATCH[1]}"
 fi
-if [ $OUTPUT =~ $purl_pattern ]; then
+if [[ $OUTPUT =~ $purl_pattern ]]; then
     purl="${BASH_REMATCH[1]}"
     #echo "url: $purl"
     echo "$purl" | pbcopy
