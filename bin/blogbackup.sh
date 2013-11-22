e3439ccbb686bce1eac701350a8e26de87e68d8f
Alex Popescu <the.mindstorm@gmail.com>
2013-11-22 03:10:19 -0800

    improved

--- bin/blogbackup.sh
+++ bin/blogbackup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 cd $HOME/Transporter/MyDocs/75-data/
 DT=$(date +"%Y%m%d-%H%M")
-tar -czf "$HOME/Transporter/MyDocs/99-archives/nosql.mypopescu.com-$DT.tar.gz" nosql.mypopescu.com/
+tar -czf "$HOME/Transporter/MyDocs/99-archives/$1-$DT.tar.gz" $1
