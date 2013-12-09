a9eee8fb2fc8ec37071f87849a85d4bf6ba79e2c
Alex Popescu <the.mindstorm@gmail.com>
2013-12-09 14:33:43 -0800

    removed Transporter dir

--- bin/blogbackup.sh
+++ bin/blogbackup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd $HOME/Transporter/MyDocs/75-data/
+cd $HOME/Documents/MyDocs/75-data/
 DT=$(date +"%Y%m%d-%H%M")
-tar -czf "$HOME/Transporter/MyDocs/99-archives/$1-$DT.tar.gz" $1
+tar -czf "$HOME/Documents/MyDocs/99-archives/$1-$DT.tar.gz" $1
