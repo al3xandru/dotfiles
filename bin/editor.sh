9acc7c7caf86f75e7b2b00fc1a9c096601f3b6f4
Alex Popescu <the.mindstorm@gmail.com>
2014-11-16 03:44:52 -0800

    bbedit is the default editor

--- bin/editor.sh
+++ bin/editor.sh
@@ -30,31 +30,71 @@ fi
 
 if [ -z "$BBEDIT_CMD" ]; then
     TXT_ED_CMD="$VIM_CMD"
+    ED_CMD="$VIM_CMD"
 else
     TXT_ED_CMD="$BBEDIT_CMD"
+    ED_CMD="$BBEDIT_CMD"
 fi
 
-SUBL_CMD=$(which subl)
-if [ -z "$SUBL_CMD" ]; then
-	ED_CMD="$TXT_ED_CMD"
+# SUBL_CMD=$(which subl)
+# if [ -z "$SUBL_CMD" ]; then
+# 	ED_CMD="$TXT_ED_CMD"
+# else
+# 	ED_CMD="$SUBL_CMD"
+# fi
+
+# Can treat directories differently
+# 
+# if [ -d "$1" ]; then
+#     _cdir=$(pwd)
+#     _ddir=$(cd "$1"; pwd)
+#     _cmd="tell application \"BBEdit\" to open \"$_ddir\""
+#     echo "Running command: $_cmd"
+#     osascript -e "$_cmd"
+#     cd "$_cdir"
+# else
+# Getting the last argument
+# (works with bash/ksh only): ${@: -1}
+# (works with bash 3.x+ only): ${!#}
+# (works with bash 3.x+ only): $BASH_ARGV
+# (works with bash 3.x+ / ksh only): ${@:$#}
+# (works with bash 3.x+ only): ${BASH_ARGV[0]}
+# (portable version): 
+# for i in $@; do :; done
+# echo "$i"
+lastarg="${BASH_ARGV[0]}"
+if [ -d "$lastarg" ]; then
+    prj_file=$(find "$lastarg" -name *.bbprojectd | head -1)
+    if [ ! -e "${prj_file}" ]; then
+        prj_file="${dirpath%/*}/${dirpath##*/}.bbprojectd"
+        if [ ! -f "${prj_file}" ]; then
+            prj_file=""
+        fi
+    fi
+    if [ -e "${prj_file}" ]; then
+        echo "Project file: ${prj_file}"
+        read -p "Do you want to open project file? " yn
+        case $yn in
+            [Yy]* ) ${ED_CMD} "${prj_file}"; exit;;
+        esac
+        ${ED_CMD} --project "$lastarg"
+    fi
 else
-	ED_CMD="$SUBL_CMD"
+    case "$lastarg" in
+        *_EDITMSG|*MERGE_MSG|*_TAGMSG )
+            ${VIM_CMD} "$lastarg"
+            ;;
+        *crontab.* )
+            ${VIM_CMD} "$lastarg"
+            ;;
+        *.md|*.markdown|*.mdown|*.mkdown )
+            ${MARKDOWN_CMD} "$lastarg"
+            ;;
+        *.txt|*.log )
+            ${TXT_ED_CMD} "$lastarg"
+            ;;
+        * )
+            ${ED_CMD} "$lastarg"
+            ;;
+    esac
 fi
-
-case "$1" in
-    *_EDITMSG|*MERGE_MSG|*_TAGMSG )
-        ${VIM_CMD} "$1"
-        ;;
-    *crontab.* )
-        ${VIM_CMD} "$1"
-        ;;
-    *.md|*.markdown|*.mdown|*.mkdown )
-        ${MARKDOWN_CMD} "$1"
-        ;;
-    *.txt|*.log )
-        ${TXT_ED_CMD} "$1"
-        ;;
-    * )
-        "$ED_CMD" "$1"
-        ;;
-esac
