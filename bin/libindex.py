64621372feaa22f062cc4836450948904dedd648
Alex Popescu <the.mindstorm@gmail.com>
2014-12-24 21:56:16 -0800

    added support for exclusions

--- bin/libindex.py
+++ bin/libindex.py
@@ -5,6 +5,8 @@ import os
 import sys
 
 
+# When indexing the archive use the following exclusions:
+# -x .jpg -x .png -x .psd -x .tiff -x .nib -x .plist -x .strings
 ALL_EXTS = ('*',)
 ARC_EXTS = ('.zip', '.tar', '.gz', '.rar', '.jar', '.tbz')
 APP_EXTS = ('.app', '.dmg', '.pkg', '.jar')
@@ -35,6 +37,8 @@ def main(opts):
 
     """
     allowed_extensions = TYPES[opts.type]
+    excluded_extensions = opts.exclude or set('.ds_store')
+
     if opts.output:
         fout = open(opts.output, 'wb')
     else:
@@ -42,11 +46,11 @@ def main(opts):
 
     with fout:
         for indir in opts.input:
-            process(indir, allowed_extensions, fout)
+            process(indir, allowed_extensions, excluded_extensions, fout)
             fout.flush()
 
 
-def process(dir, allowed_extensions, fout):
+def process(dir, allowed_extensions, excluded_extensions, fout):
     """
     Scan a directory to index it.
 
@@ -57,7 +61,7 @@ def process(dir, allowed_extensions, fout):
     """
     for root, dirs, files in os.walk(dir):
         _display = False
-        for f in [f for f in files if accept(f, allowed_extensions)]:
+        for f in [f for f in files if accept(f, allowed_extensions, excluded_extensions)]:
             if not _display:
                 fout.write(os.linesep)
                 fout.write('[' + os.path.abspath(root) + ']')
@@ -67,7 +71,7 @@ def process(dir, allowed_extensions, fout):
             fout.write(os.linesep)
 
 
-def accept(file, allowed_extensions):
+def accept(file, allowed_extensions, excluded_extensions):
     """
     Tests if the file should or should not be listed
 
@@ -75,13 +79,14 @@ def accept(file, allowed_extensions):
     :returns: TODO
 
     """
-    if allowed_extensions[0] == '*':
-        return True
-
     _, ext = os.path.splitext(file)
     ext = ext.lower()
 
-    if ext and ext in allowed_extensions:
+    if (allowed_extensions[0] == '*') and (ext not in excluded_extensions):
+        return True
+
+
+    if (ext in allowed_extensions) and (ext not in excluded_extensions):
         return True
 
     return False
@@ -91,6 +96,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('-t', '--type', action='store', required=True)
     parser.add_argument('-i', '--input', action='append')
+    parser.add_argument('-x', '--exclude', action='append')
     parser.add_argument('-o', '--output', nargs='?')
     
     opts = parser.parse_args()
