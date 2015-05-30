7f03d24c67c5074f19b4bd2848b69df3f948d314
Alex Popescu <the.mindstorm@gmail.com>
2015-05-30 05:47:47 -0700

    Improved log messages

--- install.sh
+++ install.sh
@@ -2,25 +2,9 @@
 cd "$(dirname ${BASH_SOURCE})"
 
 function doStatus() {
-    echo "1) Checking symlinks are in place"
-    echo ""
-    for f in .{aliases,exports,functions,path,prompt_bash,slate}; do
-        if [ ! -h $HOME/"${f}" ]; then
-            echo "~/${f} is NOT a symlink"
-        fi
-    done
-    if [ ! -h "$HOME/.vimrc" ]; then
-        echo "Vim ~/.vimrc is NOT a symblink"
-    fi
-    if [ ! -h "$HOME/.emacs.d/init.el" ]; then
-        echo "Emacs ~/.emacs.d/init.el is NOT a symlink"
-    fi
-
-
     arr=("." ".git" ".gitignore" ".gitmodules" ".rsyncexclude" "README.md" "bootstrap.sh" "install.sh" ".DS_Store")
     
-    echo ""
-    echo "2) Comparing dirs"
+    echo "1) Comparing dirs"
     for file in $(find . -type d -maxdepth 1 | sed 's/^\.\///'); do
         if [[ " ${arr[@]} " =~ " ${file} " ]]; then
             continue
@@ -30,19 +14,55 @@ function doStatus() {
         diff -rq $file $HOME/$file | grep -v -e 'tags' -e 'netrwhist' -e 'README.md'
     done
     echo ""
-    echo "3) Comparing files"
+
+    echo "2) Comparing files"
     echo ""
+    t=0
     for file in $(find . -type f -maxdepth 1 | sed 's/^\.\///'); do
         if [[ " ${arr[@]} " =~ " ${file} " ]]; then
             continue
         fi
         echo "Comparing $file with $HOME/$file"
         diff -q $file $HOME/$file
+        if [ 0 -ne $? ]; then
+            t=1
+        fi
     done
+    if [ 0 -eq $t ]; then
+        echo "[OK]"
+    else
+        echo "[FAIL]"
+    fi
+    echo ""
+
+    echo "3) Checking symlinks are in place"
+    t=0
+    for f in .{aliases,exports,functions,path,prompt_bash,slate}; do
+        if [ ! -h $HOME/"${f}" ]; then
+            echo "~/${f} is NOT a symlink"
+            t=1
+        fi
+    done
+    if [ ! -h "$HOME/.vimrc" ]; then
+        echo "Vim ~/.vimrc is NOT a symblink"
+        t=1
+    fi
+    if [ ! -h "$HOME/.emacs.d/init.el" ]; then
+        echo "Emacs ~/.emacs.d/init.el is NOT a symlink"
+        t=1
+    fi
+    if [ 0 -eq $t ]; then
+        echo "[OK]"
+    else
+        echo "[FAIL]"
+    fi
+    echo ""
+
+
 }
 
 function doInstall() {
-    rsync --exclude-from=.rsyncexclude -ai . $HOME
+    rsync --exclude-from=.rsyncexclude -aq . $HOME
 
     for f in .{aliases,exports,functions,path,prompt_bash,slate}; do
         proc $f
@@ -72,20 +92,19 @@ function proc() {
         diff -sq "${f}" "${hf}" &> /dev/null
 
         if [ 0 -ne $? ]; then
-            echo "File ${f} is different!"
+            echo "[FAIL] File ${f} is different!"
         else
             if [ -h "${hf}" ]; then
-                echo "Target ${f} is a symlink to $(ls -l ${hf} | awk '{print $11}')"
+                echo "[OK] Target ${f} is a symlink to $(ls -l ${hf} | awk '{print $11}')"
             else
-                echo "Target file ${f} exists, it's the same, but not a symlink"
                 rm -f "${hf}"
                 ln -s "${PWD}/${f}" "${hf}"
-                echo "Target filed removed and symlink created"
+                echo "[OK] Replaced target identical file with symlink"
             fi
         fi
     else
-        echo "Target symlink for ${f} doesn't exist"
         ln -s "${PWD}/${f}" "${hf}"
+        echo "[OK] Target symlink for ${f} created"
     fi
 }
 
