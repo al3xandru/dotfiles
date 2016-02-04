634fdde9dbcd411d76b7726f7d1b4719c7a9cefa
Alex Popescu <the.mindstorm@gmail.com>
2016-02-04 10:16:07 -0800

    fix for DayOne 2

--- bin/imdb.py
+++ bin/imdb.py
@@ -6,6 +6,7 @@ from __future__ import print_function
 import argparse
 import httplib
 import json
+import os
 import sys
 import socket
 import subprocess
@@ -30,6 +31,9 @@ _API = ('http://api.trakt.tv/movie/seen/8861c688930852cfbff18e51f195acb0',
         'alpo',
         '808a49948b398609e61e62917bd235a8c8139866')
 
+# this is required for v2
+DAYONE_JOURNAL = '~/Library/Group Containers/5U8NS4GX82.dayoneapp2/Data/Auto Import/Default Journal.dayone'
+
 
 def prepare_title(title):
   words = title.lower().split(' ')
@@ -38,7 +42,7 @@ def prepare_title(title):
     if w in ('a', 'an', 'i', 'or', 'as', 'at', 'by', 'and', 'but', 'for', 'not', 'the', 'yet', 'with'):
       continue
     swords.append(w)
-  return ' '.join(swords)    
+  return ' '.join(swords)
 
 
 # http://stackoverflow.com/questions/1966503/does-imdb-provide-an-api
@@ -58,7 +62,7 @@ def imdbapi_data(title, year=None):
   }
   if year:
     params['year'] = year
-  
+
   qs = urllib.urlencode(params)
   imdb_data = {}
 
@@ -130,10 +134,10 @@ def themoviedb_data(title, year=None):
         if match > max_match:
           movie_id = r['id']
           max_match = match
-    
+
     if not movie_id:
       return {}, 0
-    
+
     conn = httplib.HTTPConnection('api.themoviedb.org')
     conn.request('GET', "/3/movie/%s?api_key=%s" % (movie_id, params['api_key']))
     response = conn.getresponse()
@@ -189,7 +193,7 @@ def omdbapi_data(title, year=None):
     if response.status == 200:
       data = response.read()
       imdb_data = json.loads(data)
-      if imdb_data and imdb_data.has_key('imdbID'):
+      if imdb_data and 'imdbID' in imdb_data:
         imdb_data['imdb_url'] = "http://www.imdb.com/title/%s" % imdb_data['imdbID']
   finally:
     if conn:
@@ -205,10 +209,10 @@ def rotten_data(title, year=None):
   """ Try RottenTomatoes service """
   short_title = prepare_title(title)
   params = {
-    'q': short_title, 
-    'page_limit': 25, 
-    'page': 1, 
-    'apikey': '3r2avcbv2fhn6gk2nhtxeke9'  
+    'q': short_title,
+    'page_limit': 25,
+    'page': 1,
+    'apikey': '3r2avcbv2fhn6gk2nhtxeke9'
   }
   qs = urllib.urlencode(params)
   rotten_dt = {}
@@ -246,19 +250,19 @@ def rotten_data(title, year=None):
 def find_actors(d1, d2, d3):
   actors = []
   uniques = {}
-  if d1.has_key('actors'):
+  if 'actors' in d1:
     for a in d1['actors']:
-      if not uniques.has_key(a):
+      if not a in uniques:
         actors.append(a)
         uniques[a] = True
-  if d2.has_key('Actors'):
+  if 'Actors' in d2:
     for a in d2['Actors'].split(', '):
-      if not uniques.has_key(a):
+      if not a in uniques:
         actors.append(a)
         uniques[a] = True
-  if d3.has_key('abridged_cast'):
+  if 'abridged_cast' in d3:
     for a in [a['name'] for a in d3['abridged_cast']]:
-      if not uniques.has_key(a):
+      if not a in uniques:
         actors.append(a)
         uniques[a] = True
   return actors
@@ -271,7 +275,7 @@ def match_len(in_title, movie_title):
     intitle_set.add(w)
   for w in movie_title.lower().split():
     movtitle_set.add(w)
-  return len(movtitle_set.intersection(intitle_set)) 
+  return len(movtitle_set.intersection(intitle_set))
 
 
 def main(title, opts):
@@ -308,20 +312,20 @@ def main(title, opts):
   generate_output(data, opts.dayone)
 
   # track(data, opts)
-    
+
   # generate search links if needed
   print("")
   print("http://www.imdb.com/find?q=%s" % urllib.quote_plus(title))
   print("http://www.rottentomatoes.com/search/?search=%s" % urllib.quote_plus(title))
   print("http://trakt.tv/search?query=%s" % urllib.quote_plus(title))
 
-  
+
 def track(data, opts):
   """TODO: Docstring for track.
 
-  :param data: 
-  :param opts: 
-  :returns: 
+  :param data:
+  :param opts:
+  :returns:
 
   """
   if not opts.track:
@@ -364,8 +368,10 @@ def generate_output(data, to_dayone=False):
 
     try:
       print_to(tmpf, data)
+      # required for v2 and -j flag doesn't really work
+      journal_path_flag = "--journal-file=%s" % os.path.expanduser(DAYONE_JOURNAL)
       cat_cmd = subprocess.Popen(['cat', tmpf.name], stdout=subprocess.PIPE)
-      subprocess.check_call(['/usr/local/bin/dayone', 'new', '-'], stdin=cat_cmd.stdout)
+      subprocess.check_call(['/usr/local/bin/dayone', journal_path_flag  , 'new', '-'], stdin=cat_cmd.stdout)
       cat_cmd.wait()
     finally:
       tmpf.close()
@@ -425,7 +431,7 @@ def print_to(stream, data):
   stream.write(data['synopsis'].encode('utf8'))
   stream.write("\n\n")
 
-  # Posters 
+  # Posters
   if data['poster_imdb']:
     stream.write("![Poster imdb %s](%s)\n" % (data['title'], data['poster_imdb']))
   if data['poster_rotten']:
@@ -464,13 +470,12 @@ if __name__ == '__main__':
 # Sample imdbapi.org result:
 #
 # [
-#   {"runtime": ["108 min"], "rating": 6.5, "genres": ["Adventure", "Family", "Fantasy"], "rated": "PG", "language": ["English"], "title": "Alice in Wonderland", "filming_locations": "Antony House, Torpoint, Cornwall, England, UK", "imdb_url": "http://www.imdb.com/title/tt1014759/", "writers": ["Linda Woolverton", "Lewis Carroll"], "imdb_id": "tt1014759", "directors": ["Tim Burton"], "rating_count": 166594, "actors": ["Johnny Depp", "Mia Wasikowska", "Helena Bonham Carter", "Anne Hathaway", "Crispin Glover", "Matt Lucas", "Michael Sheen", "Stephen Fry", "Alan Rickman", "Barbara Windsor", "Paul Whitehouse", "Timothy Spall", "Marton Csokas", "Tim Pigott-Smith", "John Surman"], "plot_simple": "19-year-old Alice returns to the magical world from her childhood adventure, where she reunites with her old friends and learns of her true destiny: to end the Red Queen's reign of terror.", "year": 2010, "country": ["USA"], "type": "M", "release_date": 20100326, "also_known_as": ["Alice in Wonderland: An IMAX 3D Experience"]}, 
-#   {"runtime": ["75 min"], "rating": 7.4, "genres": ["Animation", "Adventure", "Family", "Fantasy", "Musical"], "rated": "G", "language": ["English"], "title": "Alice in Wonderland", "filming_locations": "Walt Disney Studios, 500 South Buena Vista Street, Burbank, California, USA", "imdb_url": "http://www.imdb.com/title/tt0043274/", "writers": ["Lewis Carroll", "Winston Hibler"], "imdb_id": "tt0043274", "directors": ["Clyde Geronimi", "Wilfred Jackson", "and 1 more credit"], "rating_count": 49240, "actors": ["Kathryn Beaumont", "Ed Wynn", "Richard Haydn", "Sterling Holloway", "Jerry Colonna", "Verna Felton", "J. Pat O'Malley", "Bill Thompson", "Heather Angel", "Joseph Kearns", "Larry Grey", "Queenie Leonard", "Dink Trout", "Doris Lloyd", "James MacDonald"], "plot_simple": "Alice stumbles into the world of Wonderland. Will she get home? Not if the Queen of Hearts has her way.", "year": 1951, "country": ["USA"], "type": "M", "release_date": 19510728, "also_known_as": ["Alicia en el pa\u00eds de las maravillas"]}, 
+#   {"runtime": ["108 min"], "rating": 6.5, "genres": ["Adventure", "Family", "Fantasy"], "rated": "PG", "language": ["English"], "title": "Alice in Wonderland", "filming_locations": "Antony House, Torpoint, Cornwall, England, UK", "imdb_url": "http://www.imdb.com/title/tt1014759/", "writers": ["Linda Woolverton", "Lewis Carroll"], "imdb_id": "tt1014759", "directors": ["Tim Burton"], "rating_count": 166594, "actors": ["Johnny Depp", "Mia Wasikowska", "Helena Bonham Carter", "Anne Hathaway", "Crispin Glover", "Matt Lucas", "Michael Sheen", "Stephen Fry", "Alan Rickman", "Barbara Windsor", "Paul Whitehouse", "Timothy Spall", "Marton Csokas", "Tim Pigott-Smith", "John Surman"], "plot_simple": "19-year-old Alice returns to the magical world from her childhood adventure, where she reunites with her old friends and learns of her true destiny: to end the Red Queen's reign of terror.", "year": 2010, "country": ["USA"], "type": "M", "release_date": 20100326, "also_known_as": ["Alice in Wonderland: An IMAX 3D Experience"]},
+#   {"runtime": ["75 min"], "rating": 7.4, "genres": ["Animation", "Adventure", "Family", "Fantasy", "Musical"], "rated": "G", "language": ["English"], "title": "Alice in Wonderland", "filming_locations": "Walt Disney Studios, 500 South Buena Vista Street, Burbank, California, USA", "imdb_url": "http://www.imdb.com/title/tt0043274/", "writers": ["Lewis Carroll", "Winston Hibler"], "imdb_id": "tt0043274", "directors": ["Clyde Geronimi", "Wilfred Jackson", "and 1 more credit"], "rating_count": 49240, "actors": ["Kathryn Beaumont", "Ed Wynn", "Richard Haydn", "Sterling Holloway", "Jerry Colonna", "Verna Felton", "J. Pat O'Malley", "Bill Thompson", "Heather Angel", "Joseph Kearns", "Larry Grey", "Queenie Leonard", "Dink Trout", "Doris Lloyd", "James MacDonald"], "plot_simple": "Alice stumbles into the world of Wonderland. Will she get home? Not if the Queen of Hearts has her way.", "year": 1951, "country": ["USA"], "type": "M", "release_date": 19510728, "also_known_as": ["Alicia en el pa\u00eds de las maravillas"]},
 #   {"runtime": ["USA: 150 min", "65 min (2 episodes)", "USA: 129 min (VHS version)", "Finland: 131 min (TV) (2 parts)"], "rating": 6.0, "genres": ["Adventure", "Comedy", "Family", "Fantasy"], "rated": "PG", "language": ["English"], "title": "Alice in Wonderland", "filming_locations": "Burnham Beeches, Buckinghamshire, England, UK", "imdb_url": "http://www.imdb.com/title/tt0164993/", "writers": ["Lewis Carroll", "Peter Barnes"], "imdb_id": "tt0164993", "directors": ["Nick Willing"], "rating_count": 4406, "actors": ["Robbie Coltrane", "Whoopi Goldberg", "Ben Kingsley", "Christopher Lloyd", "Pete Postlethwaite", "Miranda Richardson", "Martin Short", "Peter Ustinov", "George Wendt", "Gene Wilder", "Tina Majorino", "Ken Dodd", "Jason Flemyng", "Sheila Hancock", "Simon Russell Beale"], "plot_simple": "The wizards behind \"Merlin\" and \"The Odyssey\" combine Lewis Carroll's \"Alice in Wonderland\" and \"Through the Looking Glass\" into a three-hour special that just gets curiouser and curiouser.", "year": 1999, "country": ["UK", "USA", "Germany"], "type": "TV", "release_date": 19990228, "also_known_as": ["Alice im Wunderland"]}
 # ]
 #
 # Sample omdbapi.com:
 #
 # {"Title":"Lost Girl","Year":"2010","Rated":"18","Released":"12 Sep 2010","Runtime":"1 h","Genre":"Crime, Fantasy, Horror","Director":"N/A","Writer":"M.A. Lovretta","Actors":"Anna Silk, Kris Holden-Ried, Ksenia Solo, Richard Howland","Plot":"Lost Girl focuses on the gorgeous and charismatic Bo, a supernatural being called a succubus who feeds on the energy of humans...","Poster":"http://ia.media-imdb.com/images/M/MV5BMTY4NzA1MDAyMF5BMl5BanBnXkFtZTcwMzQ4MTkxNA@@._V1_SX300.jpg","imdbRating":"7.7","imdbVotes":"7,203","imdbID":"tt1429449","Response":"True"}
-
 
