80ff799e1be2dde89bb2130c858000c5be23f4a0
Alex Popescu <the.mindstorm@gmail.com>
2024-01-03 15:09:05 +0200

    changes window functions to HJKL
    introduces namespacing for my window handling functions.
    changes all window handling shortcuts to HJKL keys:
    - Ctrl-Cmd: move window in screen
    - Ctrl-Alt-Cmd: H/L move to left/right desktop
    - Ctrl-Alt-Cmd: J/K throw to left/right desktop

--- .hammerspoon/windowmanager.lua
+++ .hammerspoon/windowmanager.lua
@@ -6,23 +6,25 @@ local DEFAULT_WND_GAP = 5
 --
 -- Utility functions
 -- 
-function dynamicResizeLeft()
+AxWndManager = {}
+
+AxWndManager.dynamicResizeLeft=function()
     local win = hs.window.focusedWindow() or hs.window.frontmostWindow()
     if not win then
-        print("dynamicResizeLeft: ERROR no window found")
+        print("ERROR dynamicResizeLeft: no window found")
         return
     elseif DEBUG then   
-        wtf("dynamicResizeLeft", win)
+        AxWndManager.wtf("dynamicResizeLeft", win)
     end
 
     local frm = win:frame()
     local scr = win:screen():frame()
 
-    if not between(frm.x, scr.x, 5) then
+    if not AxWndManager.between(frm.x, scr.x, 5) then
         -- move to the left
         frm.x = scr.x
         hs.alert.show("⬅ ")
-    elseif not between(frm.y, scr.y, 25) then
+    elseif not AxWndManager.between(frm.y, scr.y, 25) then
         -- now we also resize
         -- frm.x = scr.x
         -- frm.y = scr.y
@@ -34,13 +36,13 @@ function dynamicResizeLeft()
         frm.w = scr.w * 1/2 - DEFAULT_WND_GAP
         frm.h = scr.h
         hs.alert.show("⬅  1/2")
-    elseif between(frm.w, scr.w * 2/3, DEFAULT_WND_GAP * 2) then
+    elseif AxWndManager.between(frm.w, scr.w * 2/3, DEFAULT_WND_GAP * 2) then
         frm.x = scr.x
         frm.y = scr.y
         frm.w = scr.w * 1/2 - DEFAULT_WND_GAP
         frm.h = scr.h
         hs.alert.show("⬅  1/2")
-    elseif between(frm.w, scr.w * 1/2, DEFAULT_WND_GAP * 2) then
+    elseif AxWndManager.between(frm.w, scr.w * 1/2, DEFAULT_WND_GAP * 2) then
         frm.x = scr.x
         frm.y = scr.y
         frm.w = scr.w * 1/3 - DEFAULT_WND_GAP
@@ -56,7 +58,7 @@ function dynamicResizeLeft()
     win:setFrame(frm)
 end
 
-function dynamicResizeRight()
+AxWndManager.dynamicResizeRight=function()
     local win = hs.window.focusedWindow() or hs.window.frontmostWindow()
     if not win then
         print("dynamicResizeRight: ERROR no window found")
@@ -68,11 +70,11 @@ function dynamicResizeRight()
     local frm = win:frame()
     local scr = win:screen():frame()
 
-    if not between(frm.x + frm.w, scr.x + scr.w, 5) then
+    if not AxWndManager.between(frm.x + frm.w, scr.x + scr.w, 5) then
         -- move to the left
         frm.x = scr.x + (scr.w - frm.w)
         hs.alert.show("➡")
-    elseif not between(frm.y, scr.y, 25) then
+    elseif not AxWndManager.between(frm.y, scr.y, 25) then
         -- now we also resize
         -- frm.w = scr.w * 2/3 - DEFAULT_WND_GAP
         -- frm.h = scr.h
@@ -84,13 +86,13 @@ function dynamicResizeRight()
         frm.x = scr.x + (scr.w - frm.w)
         frm.y = scr.y
         hs.alert.show("1/2 ➡")
-    elseif between(frm.w, scr.w * 2/3, DEFAULT_WND_GAP * 2) then
+    elseif AxWndManager.between(frm.w, scr.w * 2/3, DEFAULT_WND_GAP * 2) then
         frm.w = scr.w * 1/2 - DEFAULT_WND_GAP
         frm.h = scr.h
         frm.x = scr.x + (scr.w - frm.w)
         frm.y = scr.y
         hs.alert.show("1/2 ➡")
-    elseif between(frm.w, scr.w * 1/2, DEFAULT_WND_GAP * 2) then
+    elseif AxWndManager.between(frm.w, scr.w * 1/2, DEFAULT_WND_GAP * 2) then
         frm.w = scr.w * 1/3 - DEFAULT_WND_GAP
         frm.h = scr.h
         frm.x = scr.x + (scr.w - frm.w)
@@ -106,24 +108,24 @@ function dynamicResizeRight()
     win:setFrame(frm)
 end
 
-function dynamicResizeTop()
+AxWndManager.dynamicResizeTop=function()
     local win = hs.window.focusedWindow() or hs.window.frontmostWindow()
     if not win then
         print("dynamicResizeTop: ERROR no window found")
         return
     elseif DEBUG then   
-        wtf("dynamicResizeTop:", win)
+        AxWndManager.wtf("dynamicResizeTop:", win)
     end
 
     local frm = win:frame()
     local scr = win:screen():frame()
     -- UndoStack:push({wnd=win:id(), frm=frm})
 
-    if not between(frm.y, scr.y, 5) then
+    if not AxWndManager.between(frm.y, scr.y, 5) then
         -- move to the left
         frm.y = scr.y
         hs.alert.show("⬆")
-    elseif not between(frm.x, scr.x, 5) then
+    elseif not AxWndManager.between(frm.x, scr.x, 5) then
         -- now we also resize
         -- frm.x = scr.x
         -- frm.y = scr.y
@@ -135,13 +137,13 @@ function dynamicResizeTop()
         frm.w = scr.w
         frm.h = scr.h * .5 - DEFAULT_WND_GAP
         hs.alert.show("⬆ 1/2")
-    elseif between(frm.h, scr.h * .7, DEFAULT_WND_GAP * 2.5) then
+    elseif AxWndManager.between(frm.h, scr.h * .7, DEFAULT_WND_GAP * 2.5) then
         frm.x = scr.x
         frm.y = scr.y
         frm.w = scr.w
         frm.h = scr.h * .5 - DEFAULT_WND_GAP
         hs.alert.show("⬆ 1/2")
-    elseif between(frm.h, scr.h * .5, DEFAULT_WND_GAP * 2.5) then
+    elseif AxWndManager.between(frm.h, scr.h * .5, DEFAULT_WND_GAP * 2.5) then
         frm.x = scr.x
         frm.y = scr.y
         frm.w = scr.w
@@ -157,24 +159,24 @@ function dynamicResizeTop()
     win:setFrame(frm)
 end
 
-function dynamicResizeBottom()
+AxWndManager.dynamicResizeBottom=function()
     local win = hs.window.focusedWindow() or hs.window.frontmostWindow()
     if not win then
         print("dynamicResizeBottom: ERROR no window found")
         return
     elseif DEBUG then   
-        wtf("dynamicResizeBottom:", win)
+        AxWndManager.wtf("dynamicResizeBottom:", win)
     end
 
     local frm = win:frame()
     local scr = win:screen():frame()
     -- UndoStack:push({wnd=win:id(), frm=frm})
     --
-    if not between(frm.y + frm.h, scr.y + scr.h, 5) then
+    if not AxWndManager.between(frm.y + frm.h, scr.y + scr.h, 5) then
         -- move to the left
         frm.y = scr.y + scr.h - frm.h
         hs.alert.show("⬇")
-    elseif not (between(frm.x, scr.x, 5) and between(frm.w, scr.w, 25)) then
+    elseif not (AxWndManager.between(frm.x, scr.x, 5) and AxWndManager.between(frm.w, scr.w, 25)) then
         -- now we also resize
         -- frm.w = scr.w
         -- frm.h = scr.h * .7 - DEFAULT_WND_GAP
@@ -186,13 +188,13 @@ function dynamicResizeBottom()
         frm.x = scr.x
         frm.y = scr.y + scr.h - frm.h
         hs.alert.show("⬇ 1/2")
-    elseif between(frm.h, scr.h * .7, DEFAULT_WND_GAP * 2) then
+    elseif AxWndManager.between(frm.h, scr.h * .7, DEFAULT_WND_GAP * 2) then
         frm.w = scr.w
         frm.h = scr.h * .5 - DEFAULT_WND_GAP
         frm.x = scr.x
         frm.y = scr.y + scr.h - frm.h
         hs.alert.show("⬇ 1/2")
-    elseif between(frm.h, scr.h * .5, DEFAULT_WND_GAP * 2.5) then 
+    elseif AxWndManager.between(frm.h, scr.h * .5, DEFAULT_WND_GAP * 2.5) then 
         frm.w = scr.w
         frm.h = scr.h * .3 - DEFAULT_WND_GAP
         frm.x = scr.x
@@ -208,7 +210,7 @@ function dynamicResizeBottom()
     win:setFrame(frm)
 end
 
-function expandVertically()
+AxWndManager.expandVertically=function()
     local win = hs.window.focusedWindow() or hs.window.frontmostWindow()
     if not win then
         print("expandVertically: ERROR no window found")
@@ -222,7 +224,7 @@ function expandVertically()
     win:setFrame(frm)
 end
 
-function expandHorizontally()
+AxWndManager.expandHorizontally=function()
     local win = hs.window.focusedWindow() or hs.window.frontmostWindow()
     if not win then
         print("expandHorizontally: ERROR no window found")
@@ -237,19 +239,22 @@ function expandHorizontally()
 end
 
 -- Get list of screens and refresh that list whenever screens are plugged or unplugged:
+--[[
 local screens = hs.screen.allScreens()
 local screenwatcher = hs.screen.watcher.new(function()
 	screens = hs.screen.allScreens()
 end)
 screenwatcher:start()
+--]]
 -- Move window to `incr` monitors from current one 
 -- Index starts with 1
-function moveToMonitor(incr)
+AxWndManager.moveToMonitor=function(incr)
     local win = hs.window.focusedWindow() or hs.window.frontmostWindow()
     if not win then
         print("moveToMonitor: ERROR no window found")
         return
     end
+    local screens = hs.screen.allScreens()
     local scr = win:screen()
     local currentScrIdx = nil
 
@@ -278,7 +283,7 @@ end
 
 -- Move window to adjacent Desktop
 -- inspired by https://github.com/Hammerspoon/hammerspoon/issues/235
---
+--[[
 function moveWndNextSpace(direction)
     local win = hs.window.focusedWindow() or hs.window.frontmostWindow()
     if not win then
@@ -310,13 +315,81 @@ function moveWndNextSpace(direction)
         hs.eventtap.event.newMouseEvent(hs.eventtap.event.types.leftMouseUp, clickPoint):post()
     end)
 end
+--]]
+
+-- Started based on https://github.com/Hammerspoon/hammerspoon/issues/3111
+AxWndManager.moveWndToSpace = function(direction, focus)
+    local win = hs.window.focusedWindow() or hs.window.frontmostWindow()
+    if not win then
+        print("ERROR moveWndToSpace(" .. direction .. ") no window found")
+        return
+    end
+    if not win:isStandard() then
+        print("INFO  moveWndToSpace(" .. direction .. ") non-standard window")
+        AxWndManager.flashScreen(win:screen())
+        return
+    end
+    if win:isFullScreen() then
+        print("INFO  moveWndToSpace(" .. direction .. ") full-screen window")
+        AxWndManager.flashScreen(win:screen())
+        return
+    end
+    local screenId = win:screen():getUUID()
+    local screenSpaces = hs.spaces.allSpaces()[screenId]
+    local currentDesktopIdx = 0
+    -- print("INFO  moveWndToSpace: " .. screenId .. ", spaces:", screenSpaces, ", focused: " .. hs.spaces.focusedSpace())
+    for idx, d in ipairs(screenSpaces) do
+        -- print("find index of current space: " .. idx .. ", spaceIdx: " .. d)
+        if d == hs.spaces.focusedSpace() then
+            currentDesktopIdx = idx
+            -- print("found: " .. currentDesktopIdx)
+            break
+        end
+    end
+    if currentDesktopIdx == 0 then
+       print("ERROR moveWndToSpace: couldn't find index of current desktop")
+       return
+    end
+    local idx = currentDesktopIdx
+    local moved = false
+    repeat 
+        if direction == "left" then
+            idx = (idx > 1) and (idx - 1) or #screenSpaces
+        else
+            idx = (idx < #screenSpaces) and (idx + 1) or 1
+        end
+        -- print("INFO  moveWndToSpace: try space[" .. idx .. "]=" .. screenSpaces[idx]) 
+        moved = hs.spaces.moveWindowToSpace(win, screenSpaces[idx])
+    until moved or idx == currentDesktopIdx
+    if moved then
+        if focus then
+            win:focus()
+        end
+    else
+        print("INFO  moveWndToSpace: no desktops to move to")
+    end
+end
+
+AxWndManager.flashScreeni=function(screen)
+    if not screen then
+        return
+    end
+
+    local flash=hs.canvas.new(win:screen():fullFrame()):appendElements({
+        action = "fill",
+        fillColor = { alpha = 0.25, red = 1},
+        type = "rectangle"})
+    flash:show()
+    hs.timer.doAfter(.15, function () flash:delete() end)
+    hs.alert.show("Not Supported")
+end
 
 -- utilities/foundation functions
 --
 -- https://github.com/exark/dotfiles/blob/master/.hammerspoon/init.lua
 --
 -- nudge: move a window with xpos in x and ypos in y pixels
-function nudge(xpos, ypos)
+AxWndManager.nudge=function(xpos, ypos)
     local win = hs.window.focusedWindow() or hs.window.frontmostWindow()
     if not win then
         print("nudge: ERROR no window found")
@@ -332,7 +405,7 @@ end
 -- Resize window for chunk of screen.
 -- For x and y: use 0 to expand fully in that dimension, 0.5 to expand halfway
 -- For w and h: use 1 for full, 0.5 for half
-function push(x, y, w, h)
+AxWndManager.push=function(x, y, w, h)
     local win = hs.window.focusedWindow() or hs.window.frontmostWindow()
     if not win then
         print("push: ERROR no window found")
@@ -349,7 +422,7 @@ function push(x, y, w, h)
     win:setFrame(frm)
 end
 
-function resize(xpixels, ypixels)
+AxWndManager.resize=function(xpixels, ypixels)
     local win = hs.window.focusedWindow() or hs.window.frontmostWindow()
     if not win then
         print("resize: ERROR no window found")
@@ -362,7 +435,7 @@ function resize(xpixels, ypixels)
     win:setFrame(frm)
 end
 
-function wtf(msg, wnd)
+AxWndManager.wtf=function(msg, wnd)
     if not wnd then
         print(msg ..  "(no wnd)")
         return
@@ -374,7 +447,7 @@ function wtf(msg, wnd)
     print(msg .. "Screen: [x:" .. scr.x .. ", y:" .. scr.y .. ", w:" .. scr.w .. ", h:" .. scr.h ..  "]")
 end
 
-function between(value, reference, delta)
+AxWndManager.between=function(value, reference, delta)
     if value >= (reference - delta) and value <= (reference + delta) then
         if DEBUG then
             print("TRUE  " .. value .. " in [" .. (reference - delta) .. ", " .. (reference + delta) .. "]")
@@ -390,60 +463,63 @@ end
 --
 -- Bindings
 --
-local alt_cmd = {"⌘", "⌥"}
-local alt_shift_cmd = {"⌥", "⇧", "⌘"}
-local ctrl_alt_cmd = {"⌥", "⌃", "⌘"}
-local cmd_ctrl = {"cmd", "ctrl"}
+-- Control ^, Option, Shift, Command
+local ctrl_cmd = {"ctrl", "cmd"}
+local ctrl_opt = {"ctrl", "alt"}
+local ctrl_opt_cmd = {"ctrl", "alt", "cmd"}
+local opt_cmd = {"alt", "cmd"}
+local ctrl_opt_shift = {"ctrl", "alt", "shift"}
 
 -- Snap to screen edge {{{1
 -- Raycast introduced this behavior in 1.43.0
-hs.hotkey.bind(alt_cmd, "left",  dynamicResizeLeft)
-hs.hotkey.bind(alt_cmd, "right", dynamicResizeRight)
-hs.hotkey.bind(alt_cmd, "up",    dynamicResizeTop)
-hs.hotkey.bind(alt_cmd, "down",  dynamicResizeBottom)
-hs.hotkey.bind(cmd_ctrl, "h", "◀️", dynamicResizeLeft)
-hs.hotkey.bind(cmd_ctrl, "l", "▶️", dynamicResizeRight)
-hs.hotkey.bind(cmd_ctrl, "k", "🔼", dynamicResizeTop)
-hs.hotkey.bind(cmd_ctrl, "j", "🔽", dynamicResizeBottom)
+hs.hotkey.bind(opt_cmd, "left",  AxWndManager.dynamicResizeLeft)
+hs.hotkey.bind(opt_cmd, "right", AxWndManager.dynamicResizeRight)
+hs.hotkey.bind(opt_cmd, "up",    AxWndManager.dynamicResizeTop)
+hs.hotkey.bind(opt_cmd, "down",  AxWndManager.dynamicResizeBottom)
+hs.hotkey.bind(ctrl_cmd, "h", "◀️", AxWndManager.dynamicResizeLeft)
+hs.hotkey.bind(ctrl_cmd, "l", "▶️", AxWndManager.dynamicResizeRight)
+hs.hotkey.bind(ctrl_cmd, "k", "🔼", AxWndManager.dynamicResizeTop)
+hs.hotkey.bind(ctrl_cmd, "j", "🔽", AxWndManager.dynamicResizeBottom)
 -- }}}
 
 -- Predefined positions:
 -- Raycast supports Center
-function center()
+AxWndManager.center=function()
     local wnd = hs.window.focusedWindow() or hs.window.frontmostWindow() 
     if wnd then
         wnd:centerOnScreen() 
     end
 end
-hs.hotkey.bind(alt_cmd, ".", "", center)
-hs.hotkey.bind(cmd_ctrl, "z", ".", center)
+hs.hotkey.bind(ctrl_cmd, ".", "", AxWndManager.center)
+hs.hotkey.bind(ctrl_cmd, "z", ".", AxWndManager.center)
 
 -- Raycast -- Enlarged vertically
-hs.hotkey.bind(cmd_ctrl, "\\", expandVertically)
+hs.hotkey.bind(ctrl_cmd, "\\", AxWndManager.expandVertically)
 -- -- Enlarged horizontally
-hs.hotkey.bind(cmd_ctrl, "=", expandHorizontally)
+hs.hotkey.bind(ctrl_cmd, "=", AxWndManager.expandHorizontally)
 -- -- Fullscreen
-hs.hotkey.bind(ctrl_alt_cmd, "f", function() push(0.01, 0.01, .95, .95) end)
+hs.hotkey.bind(ctrl_opt_cmd, "f", function() push(0.01, 0.01, .95, .95) end)
 
 -- Raycast works better for Move to different monitor
-hs.hotkey.bind(ctrl_alt_cmd, "s", function() moveToMonitor(-1) end)
--- hs.hotkey.bind(cmd_ctrl, "b", function() moveToMonitor(1) end)
+hs.hotkey.bind(ctrl_opt_cmd, "s", function() AxWndManager.moveToMonitor(-1) end)
 
 -- Move to left/right dekstop
 -- Raycast works better for these
--- hs.hotkey.bind({"alt", "ctrl"}, 'right', function() moveWndNextSpace('right') end)
--- hs.hotkey.bind({"alt", "ctrl"}, 'left', function() moveWndNextSpace('left') end)
+hs.hotkey.bind(ctrl_opt_cmd, "l", function() AxWndManager.moveWndToSpace('right', true) end)
+hs.hotkey.bind(ctrl_opt_cmd, "h", function() AxWndManager.moveWndToSpace('left', true) end)
+hs.hotkey.bind(ctrl_opt_cmd, "k", function() AxWndManager.moveWndToSpace('right', false) end)
+hs.hotkey.bind(ctrl_opt_cmd, "j", function() AxWndManager.moveWndToSpace('left', false) end)
 
 
 -- -- Move
--- hs.hotkey.bind(alt_shift_cmd, 'down',  function() nudge(0, 50) end)
--- hs.hotkey.bind(alt_shift_cmd, "up",    function() nudge(0,-50) end)
--- hs.hotkey.bind(alt_shift_cmd, "right", function() nudge(50,0) end)
--- hs.hotkey.bind(alt_shift_cmd, "left",  function() nudge(-50, 0) end)
+-- hs.hotkey.bind(alt_shift_cmd, 'down',  function() AxWndManager.nnudge(0, 50) end)
+-- hs.hotkey.bind(alt_shift_cmd, "up",    function() AxWndManager.nudge(0,-50) end)
+-- hs.hotkey.bind(alt_shift_cmd, "right", function() AxWndManager.nudge(50,0) end)
+-- hs.hotkey.bind(alt_shift_cmd, "left",  function() AxWndManager.nudge(-50, 0) end)
 
--- hs.hotkey.bind(ctrl_alt_cmd, 'up',    function() resize(0, -50) end)
--- hs.hotkey.bind(ctrl_alt_cmd, 'down',  function() resize(0, 50) end)
--- hs.hotkey.bind(ctrl_alt_cmd, 'right', function() resize(50, 0) end)
--- hs.hotkey.bind(ctrl_alt_cmd, 'left',  function() resize(-50, 0) end)
+-- hs.hotkey.bind(ctrl_opt_cmd, 'up',    function() AxWndManager.resize(0, -50) end)
+-- hs.hotkey.bind(ctrl_opt_cmd, 'down',  function() AxWndManager.resize(0, 50) end)
+-- hs.hotkey.bind(ctrl_opt_cmd, 'right', function() AxWndManager.resize(50, 0) end)
+-- hs.hotkey.bind(ctrl_opt_cmd, 'left',  function() AxWndManager.resize(-50, 0) end)
 
 
